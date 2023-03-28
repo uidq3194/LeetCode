2c0e15296b4cbf7b6ee26e632576b5abe3f71a44
uidq3194 <neculai.anghel@outlook.com>
2023-03-28 13:16:30 +0300

    Ex2

---
+++ Ex2/Ex2.py
@@ -0,0 +1,54 @@
+'''
+Link :https://leetcode.com/problems/two-sum/
+
+Title : 1. Two Sum   
+
+Description:
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    You can return the answer in any order.
+
+Example 1:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+
+Example 3:
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+    
+Constraints:
+    2 <= nums.length <= 104
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+    Only one valid answer exists.
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        result = []
+        size = len(nums)
+        for i in range(0,size):
+            for j in range(i+1,size):
+                if nums[i]+nums[j] == target:
+                        result.append(i)
+                        result.append(j)
+                        break
+        return result
+    
+problem = Solution()
+
+print(problem.twoSum([2,7,11,15],9))
+print(problem.twoSum([3,2,4],6))
+print(problem.twoSum([3,3],6))
